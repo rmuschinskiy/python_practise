29e67b5d12538ad773a1a78322c0cc82b8c7cff5
Roma M <romanintouch@gmail.com>
2017-07-16 15:33:59 +0300

    q

--- 2610.py
+++ 2610.py
@@ -1,21 +1,11 @@
 matrix = []
-while True:
-    user_input = [i for i in input().split()]
-    if user_input[0] == 'end':
-        break
+user_input = input()
+while user_input != 'end':
+    user_input = [int(i) for i in user_input.split()]
     matrix.append(user_input)
-# for i in range(len(matrix[0])):
-#     for j in range(len(matrix)):
-#         matrix[i][j] = int(matrix[i][j])
-
+    user_input = input()
 for i in range(len(matrix[0])):
     print()
     for j in range(len(matrix)):
         print(matrix[i - 1][j] + matrix[i + 1 - len(matrix)][j] + matrix[i][j - 1] + matrix[i][j + 1 - len(matrix[0])], end=' ')
-
-
-# for i in range(len(matrix[0])):
-#     print()
-#     for j in range(len(matrix)):
-#         print(matrix[i][j], end=' ')
 
